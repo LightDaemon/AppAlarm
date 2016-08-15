4ccdb01468311e54fc542fc72a318b93404d374d
ZY <zy591522686@163.com>
2016-08-15 10:43:42 +0800

    初始化；布局文件添加SimpleDraweeView

--- app/src/main/java/com/rokid/alarm1/MyApplication.java
+++ app/src/main/java/com/rokid/alarm1/MyApplication.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 /**
  * Created by ZY on 2016/6/14.
  * Description :继承MultiDexApplication，解决64k问题
@@ -14,5 +16,6 @@ public class MyApplication extends MultiDexApplication {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+        Fresco.initialize(this);         //fresco初始化
     }
 }
